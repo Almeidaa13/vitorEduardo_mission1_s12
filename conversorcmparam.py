016cf8fa2fad6cd3d86e8ab61ed092143d49baab
Vitor Almeida <V.Eduardo.almeida@hotmail.com>
2024-06-25 18:53:33 -0300

    arquivo pronto

---
+++ conversorcmparam.py
@@ -0,0 +1,9 @@
+class ConversorMedidas:
+    def _init_(self):
+        pass
+
+    def cm_to_m(cm):
+     return cm / 100
+
+    def m_to_cm(m):
+     return m * 10
---
+++ conversortemp.py
@@ -0,0 +1,9 @@
+class ConversorTemperatura:
+    def _init_(self):
+        pass
+
+    def celsius_to_fahrenheit(celsius):
+     return (celsius * 9/5) + 32
+
+    def fahrenheit_to_celsius(fahrenheit):
+     return (fahrenheit - 32) * 5/9
